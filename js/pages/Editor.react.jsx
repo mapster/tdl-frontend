125c3db373c64564fa911efe7b2b6cb120e996ff
mapster <alexander@rosbach.no>
2015-07-12 17:08:37 +0200

    added Aced editor

---
+++ js/pages/Editor.react.jsx
@@ -0,0 +1,20 @@
+/*jslint node: true */
+'use strict';
+
+var React = require('react');
+var AceEditor = require('react-ace');
+
+require('brace/mode/java');
+require('brace/theme/eclipse');
+
+var Editor = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <AceEditor mode="java" theme="eclipse" name="editor"/>
+      </div>
+    );
+  }
+});
+
+module.exports = Editor;
--- js/routes.react.jsx
+++ js/routes.react.jsx
@@ -7,11 +7,13 @@ var Route = Router.Route;
 
 var App = require('./pages/App.react');
 var Stopwatch = require('./pages/Stopwatch.react');
+var Editor = require('./pages/Editor.react');
 var Exercises = require('./pages/Exercises.react');
 
 module.exports = (
   <Route handler={App}>
     <Route name="stopwatch" path="/stopwatch" handler={Stopwatch} />
     <Route name="exercises" path="/exercises" handler={Exercises} />
+    <Route name="editor" path="/editor" handler={Editor} />
   </Route>
 );
