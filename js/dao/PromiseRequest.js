90a13756b30a94e7d79908927f1d567b16e2c2d9
mapster <alexander@rosbach.no>
2015-09-15 18:26:18 +0200

    Generalize promiseRequest

---
+++ js/dao/PromiseRequest.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var $ = require('jquery');
+
+module.exports = (function() {
+  var _promises = [];
+  function _promiseFulfilled(promiseID, callback) {
+    return function(arg1, arg2, arg3) {
+      delete _promises[promiseID];
+      callback(arg1, arg2, arg3);
+    };
+  }
+
+  return function(promiseID, requestSettings) {
+    if (!_promises[promiseID]) {
+      _promises[promiseID] = new Promise((resolve, reject) => {
+        requestSettings.success = _promiseFulfilled(promiseID, resolve);
+        requestSettings.error = _promiseFulfilled(promiseID, reject);
+        $.ajax(requestSettings);
+      });
+    }
+    return _promises[promiseID];
+  };
+})();
--- js/dao/SessionDAO.js
+++ js/dao/SessionDAO.js
@@ -1,19 +1,15 @@
 'use strict';
 
-//var Promise = require('es6-promise').Promise;
-var $ = require('jquery');
-
 var AppConfig = require('../appconfig.json');
+var promiseRequest = require('./PromiseRequest');
 
 var LOGIN = 'login';
 var LOGOUT = 'logout';
 var GET = 'get';
 var RESOURCE_URL = AppConfig.Host + '/user_session';
 
-var _promises = [];
-
 function login(credentials) {
-  return _promiseRequest(LOGIN, {
+  return promiseRequest(LOGIN, {
     type: 'POST',
     url: RESOURCE_URL,
     contentType: 'application/json',
@@ -22,35 +18,17 @@ function login(credentials) {
 }
 
 function logout() {
-  return _promiseRequest(LOGOUT, {
+  return promiseRequest(LOGOUT, {
     type: 'DELETE',
     url: RESOURCE_URL
   });
 }
 
 function getSession() {
-  return _promiseRequest(GET, {
+  return promiseRequest(GET, {
     type: 'GET',
     url: RESOURCE_URL
   });
-}
-
-function _promiseRequest(promiseID, requestSettings) {
-  if (!_promises[promiseID]) {
-    _promises[promiseID] = new Promise((resolve, reject) => {
-      requestSettings.success = _promiseFulfilled(promiseID, resolve);
-      requestSettings.error = _promiseFulfilled(promiseID, reject);
-      $.ajax(requestSettings);
-    });
-  }
-  return _promises[promiseID];
-}
-
-function _promiseFulfilled(promiseID, callback) {
-  return function(arg1, arg2, arg3) {
-    delete _promises[promiseID];
-    callback(arg1, arg2, arg3);
-  };
 }
 
 module.exports = {
