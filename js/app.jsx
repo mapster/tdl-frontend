5c7c2ae77278ca923262894fdd5113fb3dfade4b
mapster <alexander@rosbach.no>
2017-08-05 00:00:18 +0200

    Add persistedState

--- js/app.jsx
+++ js/app.jsx
@@ -8,9 +8,15 @@ import * as AppActions from './actions/app';
 import configureStore from './store/configureStore';
 import {SELECTORS} from './reducers/index';
 
+const STORAGE_KEY = 'tdl_state';
 
+const persistedState = localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY)) : {};
 const history = createBrowserHistory();
-const store = configureStore({}, history);
+const store = configureStore(persistedState, history);
+store.subscribe(() => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(store.getState()));
+});
+
 store.dispatch(AppActions.initialize(SELECTORS.router.getLocation(store.getState())));
 
 ReactDOM.render(
